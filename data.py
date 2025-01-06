36017efcb6fd538bb255d76ad4ea222c43f477b7
adityaraju <aditya.raju.2005@gmail.com>
2025-01-06 23:18:53 +0530

    csv gen

--- data.py
+++ data.py
@@ -1,6 +1,27 @@
 import kagglehub
+import os
+import csv
 
 # Download latest version
-path = kagglehub.dataset_download("ejlok1/toronto-emotional-speech-set-tess")
+# path = kagglehub.dataset_download("ejlok1/toronto-emotional-speech-set-tess")
 
-print("Path to dataset files:", path)
+# print("Path to dataset files:", path)
+
+
+dataset_dir = "Dataset"
+output_csv = "dataset_metadata.csv"
+
+def create_csv_from_folders(dataset_dir, output_csv):
+
+    with open(output_csv, mode="w", newline="", encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["filepath", "label"])
+        
+        for root, _, files in os.walk(dataset_dir):
+            label = os.path.basename(root)
+            
+            for file in files:
+                filepath = os.path.join(root, file)
+                writer.writerow([filepath, label])
+
+create_csv_from_folders(dataset_dir, output_csv)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.utils.data import Dataset
 
 device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 print(device)
 
 #params
 batch_size = 8
+
 
